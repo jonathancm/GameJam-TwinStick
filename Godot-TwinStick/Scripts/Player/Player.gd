b059bb7b0837698e5fc9b10a045282ff958151ea
Jonathan Caron <jonathancm@outlook.com>
2020-04-19 13:45:05 -0400

    Bombs are now instanced on all clients

--- Godot-TwinStick/Scripts/Player/Player.gd
+++ Godot-TwinStick/Scripts/Player/Player.gd
@@ -20,6 +20,8 @@ export(Resource) var bomb_prefab
 puppet var net_position:Vector3
 puppet var net_rotation:Quat
 
+var bombID = 0;
+
 
 
 
@@ -28,55 +30,70 @@ func _ready():
 	pass
 
 func _physics_process(dt:float):
-	
+
 	if(is_network_master()):
 		_physics_process_master(dt)
 		return
-		
+
 	global_transform = Transform(Basis(net_rotation), net_position)
-	
-	
+
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 master func _physics_process_master(dt:float):
-	
-	
+
+
 	var direction:Vector3 = mouse_direction
 	direction.y = 0
-	
+
 	if(direction.length() > 0.1):
 		transform = transform.looking_at(global_transform.origin + direction, Vector3.UP)
-	
-	
+
+
 	var targetVelocity = max_speed * left_stick.normalized()
-	
+
 	velocity = velocity.linear_interpolate(
-					Vector3(targetVelocity.x, velocity.y, targetVelocity.y), 
+					Vector3(targetVelocity.x, velocity.y, targetVelocity.y),
 					dt*(1.0/inertia))
-					
+
 	velocity.y -= gravity*dt
-	
+
 	if(is_on_floor() && jump):
 		velocity.y = jump_velocity
-	
+
 	velocity = move_and_slide(velocity, Vector3.UP)
-	
+
 	rset("net_position", global_transform.origin)
 	rset("net_rotation", global_transform.basis.get_rotation_quat())
-	
-	
+
+
 	if(shoot):
+		bombID += 1
 		var gunSocket = get_node("GunSocket")
-		var instance = bomb_prefab.instance()
-		instance.set_global_transform(gunSocket.get_global_transform())
-		get_parent().add_child(instance)
-		var impulseStrength = 2.0
-		var impulseDirection = -gunSocket.get_global_transform().basis.z
-		instance.apply_impulse(Vector3.ZERO, impulseStrength*impulseDirection)
-		instance.apply_torque_impulse (impulseStrength/20.0*Vector3.LEFT)
-	
-	
+		var transform = gunSocket.get_global_transform()
+		var bombName = "Bomb" + "_" + get_name() + "_" + str(bombID)
+		spawn_bomb(bombName, transform)
+		for p in networkController.players:
+			rpc_id(p, "spawn_bomb", bombName, transform)
+
+
+
+
+
+
+remote func spawn_bomb(name:String, transform:Transform):
+	var impulseStrength = 2.0
+	var impulseDirection = -transform.basis.z
+	var instance = bomb_prefab.instance()
+	instance.set_name(name)
+	instance.set_global_transform(transform)
+	instance.set_network_master(get_network_master())
+	get_parent().add_child(instance)
+	instance.apply_impulse(Vector3.ZERO, impulseStrength*impulseDirection)
+	instance.apply_torque_impulse (impulseStrength/20.0*Vector3.LEFT)
+
+
 
 
 func _on_update_controls(_left_stick:Vector2, _mouse_direction:Vector3, _jump:bool, _shoot:bool):
@@ -84,4 +101,4 @@ func _on_update_controls(_left_stick:Vector2, _mouse_direction:Vector3, _jump:bo
 	left_stick = _left_stick
 	jump = _jump
 	shoot = _shoot
-	
+
